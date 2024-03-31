7b79cdff4e9143e4732c7a57ade727e0af3a04f0
Leonardo <leonardomainardes168@gmail.com>
2024-03-31 16:44:32 -0300

    Criando a classe do separador com seus métodos/funções

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Separador{
+    dispositivo: dispositivo::Dispositivo,
+}
+
+impl Separador{
+    fn adicionarArquivo(){
+
+    }
+    fn removerArquivo(){
+
+    }
+    fn organizarArquivo(){
+
+    }
+    fn buscarArquivos(){
+
+    }
+}
+
+mod dispositivo{
+    pub struct Dispositivo{
+
+    }
+}
